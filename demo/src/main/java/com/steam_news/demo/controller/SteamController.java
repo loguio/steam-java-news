d239641872e6cfb504b2547a0283ee85d2e6662e
Clément Farina <122206754+Ligax45@users.noreply.github.com>
2024-08-19 14:40:46 +0200

    first methode service + controller

--- demo/src/main/java/com/steam_news/demo/controller/SteamController.java
+++ demo/src/main/java/com/steam_news/demo/controller/SteamController.java
@@ -18,7 +18,7 @@ public class SteamController {
         this.steamService = steamService;
     }
 
-    @GetMapping
+    @GetMapping("top100")
     public ResponseEntity<String> getPopularGames(@RequestParam(required = false, defaultValue = "top100in2weeks") String request) {
         try {
             String formattedJson = steamService.fetchPopularGames(request);
@@ -27,5 +27,17 @@ public class SteamController {
             System.err.println("Erreur lors de l'appel à l'API Steam : " + e.getMessage());
             return ResponseEntity.status(500).body("Erreur lors de la récupération des données de l'API Steam.");
         }
+    }
+
+    @GetMapping("details")
+    public ResponseEntity<String> getDetailsGame(int id) {
+        return null;
+        // try {
+        //     String getDetails = steamService.GetDetailsGame(id);
+        //     return getDetails;
+        // } catch (Exception e) {
+        //      System.err.println("Erreur lors de l'appel à l'API Steam : " + e.getMessage());
+        //     return ResponseEntity.status(500).body("Erreur lors de la récupération des données de l'API Steam.");
+        // }
     }
 }
--- demo/src/main/java/com/steam_news/demo/service/SteamService.java
+++ demo/src/main/java/com/steam_news/demo/service/SteamService.java
@@ -8,7 +8,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class SteamService {
-     private final RestTemplate restTemplate;
+    private final RestTemplate restTemplate;
     private final ObjectMapper objectMapper;
 
     public SteamService(RestTemplate restTemplate, ObjectMapper objectMapper) {
@@ -16,6 +16,7 @@ public class SteamService {
         this.objectMapper = objectMapper;
     }
 
+    // FetchPopularGames
     public String fetchPopularGames(String request) throws Exception {
         String url = "https://steamspy.com/api.php?request=" + request;
         
@@ -24,6 +25,15 @@ public class SteamService {
         
         // Parser et formater le JSON
         JsonNode jsonNode = objectMapper.readTree(responseBody);
+        System.out.println(jsonNode.get("570").get("name"));
         return objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(jsonNode);
+    }
+
+    public String GetDetailsGame(int id) {
+        String url = "https://store.steampowered.com/api/appdetails?appids=" + id;
+
+        String responseBody = restTemplate.getForObject(url, String.class);
+
+        return responseBody;
     }
 }
