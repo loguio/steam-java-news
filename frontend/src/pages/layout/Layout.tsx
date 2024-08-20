b00ff6d1383f031ac6954988db375b5bb9c565db
Bourse Marius <marius.bourse@hotmail.fr>
2024-08-20 11:59:37 +0200

    begining layout

--- frontend/src/pages/layout/Layout.tsx
+++ frontend/src/pages/layout/Layout.tsx
@@ -3,6 +3,48 @@ interface ReactNodeProps {
 }
 
 const Layout = (props: ReactNodeProps) => {
-  return <>{props.children}</>;
+  return (
+    <>
+      <div
+        style={{
+          height: "120px",
+          width: "100%",
+          backgroundColor: "lightblue",
+          alignItems: "center",
+          display: "flex",
+        }}
+      >
+        <div
+          onClick={() => null}
+          style={{
+            height: "50px",
+            width: "150px",
+            backgroundColor: "lightpink",
+            justifyContent: "center",
+            alignItems: "center",
+            display: "flex",
+            marginLeft: "150px",
+          }}
+        >
+          Popular Game
+        </div>
+        <div
+          onClick={() => null}
+          style={{
+            height: "50px",
+            width: "150px",
+            backgroundColor: "lightpink",
+            justifyContent: "center",
+            alignItems: "center",
+            display: "flex",
+            marginLeft: "150px",
+          }}
+        >
+          Discount Game
+        </div>
+      </div>
+      {props.children}
+    </>
+  );
 };
 export default Layout;
