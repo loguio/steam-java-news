22de85220707ed0ac1349721f3377ed48aa83a4e
Clément Farina <122206754+Ligax45@users.noreply.github.com>
2024-08-20 14:56:13 +0200

    discount

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -14,3 +14,15 @@ export const getGames = async (page: number = 0): Promise<model[] | null> => {
     return null;
   }
 };
+
+export const getDetails = async (id: number): Promise<model[] | null> => {
+  try {
+    const response = await axios.get(
+      "http://localhost:8080/steam_api/details?id=" + id
+    );
+    return response.data as model[];
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+};
--- frontend/src/pages/Discount.tsx
+++ frontend/src/pages/Discount.tsx
@@ -1,3 +1,6 @@
+import { getDetails } from "../api/api";
+
 export const Discount = () => {
+  getDetails(560);
   return <h1>Page discount</h1>;
 };
